2f8c4d2654bea8a005b10c133dc0a15bbf33fedd
kanounfoued <fouad.kanoun@ecoreasoning.com>
2023-08-06 23:56:57 +0100

    style: build button design system

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+type ButtonProps = {
+  label?: string;
+  type?: "text" | "primary" | "secondary";
+  className?: string;
+  onClick?: () => void;
+};
+
+export default function Button({
+  label,
+  type,
+  className = "",
+  onClick,
+}: ButtonProps) {
+  if (type === "text") {
+    return (
+      <button
+        className={`px-6 py-[0.7rem] rounded-md  ${className}`}
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    );
+  }
+
+  if (type === "primary") {
+    return (
+      <button
+        className={`px-6 py-[0.7rem] rounded-md bg-[--main-color]  ${className}`}
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    );
+  }
+
+  if (type === "secondary") {
+    return (
+      <button
+        className={`px-6 py-[0.7rem] rounded-md border ${className}`}
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    );
+  }
+}
