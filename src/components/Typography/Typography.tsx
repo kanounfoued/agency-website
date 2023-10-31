ebb3b4aa5211ffc0c637852ade45aa921eb4190d
kanounfoued <fouad.kanoun@ecoreasoning.com>
2023-10-31 22:53:32 +0100

    feat: add style property to Typography component

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -17,36 +17,82 @@ type TypographyProps = {
   level?: LevelProps;
   children: React.ReactNode;
   className?: string;
+  style?: React.CSSProperties;
 };
 
 export default function Typography({
   level = LevelProps.text,
   children,
   className = "",
+  style,
 }: TypographyProps) {
   switch (level) {
     case "h1":
-      return <h1 className={`h1 ${className}`}>{children}</h1>;
+      return (
+        <h1 style={style} className={`h1 ${className}`}>
+          {children}
+        </h1>
+      );
     case "h2":
-      return <h2 className={`h2 ${className}`}>{children}</h2>;
+      return (
+        <h2 style={style} className={`h2 ${className}`}>
+          {children}
+        </h2>
+      );
     case "h3":
-      return <h3 className={`h3 ${className}`}>{children}</h3>;
+      return (
+        <h3 style={style} className={`h3 ${className}`}>
+          {children}
+        </h3>
+      );
     case "h4":
-      return <h4 className={`h4 ${className}`}>{children}</h4>;
+      return (
+        <h4 style={style} className={`h4 ${className}`}>
+          {children}
+        </h4>
+      );
     case "h5":
-      return <h5 className={`h5 ${className}`}>{children}</h5>;
+      return (
+        <h5 style={style} className={`h5 ${className}`}>
+          {children}
+        </h5>
+      );
     case "label":
-      return <label className={`label ${className}`}>{children}</label>;
+      return (
+        <label style={style} className={`label ${className}`}>
+          {children}
+        </label>
+      );
     case "big_text":
-      return <p className={`big_text ${className}`}>{children}</p>;
+      return (
+        <p style={style} className={`big_text ${className}`}>
+          {children}
+        </p>
+      );
     case "text":
-      return <p className={`text ${className}`}>{children}</p>;
+      return (
+        <p style={style} className={`text ${className}`}>
+          {children}
+        </p>
+      );
     case "small_text":
-      return <p className={`small_text ${className}`}>{children}</p>;
+      return (
+        <p style={style} className={`small_text ${className}`}>
+          {children}
+        </p>
+      );
     case "gray_text":
-      return <p className={`gray_text ${className}`}>{children}</p>;
+      return (
+        <p style={style} className={`gray_text ${className}`}>
+          {children}
+        </p>
+      );
 
     default:
-      return <p className={`text ${className}`}>{children}</p>;
+      return (
+        <p style={style} className={`text ${className}`}>
+          {children}
+        </p>
+      );
   }
 }
